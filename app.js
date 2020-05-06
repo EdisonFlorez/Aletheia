03365593a6668599a62e737a047e5396847ad25d
E-FLorezB <eflorez@icloud.com>
2020-05-05 20:04:27 -0500

    Inicializing

---
+++ app.js
@@ -0,0 +1,18 @@
+let elemento;
+/*elemento = document.querySelector(".bm-color-blue").querySelector(".bm-text").childNodes[1].innerText
+*/
+elemento = document.querySelectorAll(".bm-color-blue")
+
+var arrayelement = Array.from(elemento);
+var arraywords = new Array();
+function putValues(){
+    for(let i=0;i<arrayelement.length;i++){
+        let palabra = arrayelement[i].querySelector(".bm-text").innerText
+        
+        arraywords[i] = palabra;
+    }
+}
+putValues();
+
+
+console.log(arraywords);
